3b8c94cb5d5a3b996164ce1bfa913b933c8cc607
saihcodes <111911090+saihcodes@users.noreply.github.com>
2022-09-13 21:34:10 +0000

    Sep 13, 2022, 5:34 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -54,8 +54,7 @@ public class PlayerController : MonoBehaviour
         if (lives == 0)
         {
             LoseTextObject.SetActive(true);
-            Destroy(gameObject)
- 
+                
 
         }
     }
