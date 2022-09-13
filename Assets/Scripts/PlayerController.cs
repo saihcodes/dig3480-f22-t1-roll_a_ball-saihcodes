bb95ec8d5d787ff022765a93dc039eb5368702a8
saihcodes <111911090+saihcodes@users.noreply.github.com>
2022-09-13 17:15:41 +0000

    Sep 13, 2022, 1:15 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -57,7 +57,12 @@ public class PlayerController : MonoBehaviour
 
             SetCountText();
         }
-
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            SetCountText();
+        }
     }
 }
 
